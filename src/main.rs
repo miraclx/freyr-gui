b4afd91308cd6c852c6b848d07326b03976206fa
Miraclx <omiraculous@gmail.com>
2021-01-01 23:33:47 +0100

    Add base working impl for launching UI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use qt_ui_tools::ui_form;
+use qt_widgets::{qt_core::QBox, QApplication, QWidget};
+use std::rc::Rc;
+
+#[ui_form("../ui/main.ui")]
+struct MainWindow {
+    widget: QBox<QWidget>,
+}
+
+struct FreyrGUI {
+    main_window: MainWindow,
+}
+
+impl FreyrGUI {
+    fn new() -> Rc<Self> {
+        let this = Rc::new(FreyrGUI {
+            main_window: MainWindow::load(),
+        });
+        unsafe { this.init() }
+        this
+    }
+    unsafe fn init(self: &Rc<Self>) {}
+    fn show(self: &Rc<Self>) {
+        unsafe { self.main_window.widget.show() }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    QApplication::init(|_| {
+        let freyr_gui = FreyrGUI::new();
+        freyr_gui.show();
+        unsafe { QApplication::exec() }
+    })
 }
